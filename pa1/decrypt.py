7fb5b4a808e191ec96a66195ff5bc9255af13d38
Michael Graczyk <michael@mgraczyk.com>
2014-01-22 03:50:51 -0600

    Updated ps1 scrypt. Use python 2.7

--- pa1/decrypt.py
+++ pa1/decrypt.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python2.7
 
 from ct import *
 
@@ -12,9 +12,11 @@ def compose(f, g):
     return composition
 
 def xor_could_be_space(ch):
-    return ord(ch[1]) == 0 or ord(ch[1]) >= 65
+    #return ord(ch[1]) == 0 or ord(ch[1]) >= 65
+    return ord(ch[1]) >= 65
 
 def do_decrypt():
+    keyLen = max(len(a) for a in cts)
     crossXors = [[strxor(a,b) for b in cts] for a in cts]
 
     spaceIndices = map(
@@ -28,8 +30,19 @@ def do_decrypt():
                             partial(filter, xor_could_be_space)),
                         enumerate)))),
         crossXors)
+
+    key = ["\x00" for i in xrange(keyLen)]
+
+    for (ct, indices) in zip(cts, spaceIndices):
+        print("KEY: " + "".join(key).encode('hex'))
+        for i in indices:
+            key[i] = strxor(" ", ct[i])
+
+    targetStr = cts[-1]
+    targetPT = encrypt("".join(key)[0:len(targetStr)], targetStr)
+
+    print("".join(targetPT[i] if key[i] else "_" for i in xrange(len(targetPT))))
    
-    print([indices for indices in spaceIndices])
 
 if __name__ == "__main__":
     do_decrypt()
